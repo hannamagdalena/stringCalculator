e9a35b42cfa5670911acb7eb4867fb4dae36a6cb
hannamagdalena <hanna_schweizer@gmx.at>
2019-02-11 12:59:17 +0100

    Fertig

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -23,7 +23,7 @@ describe ('StringCalculator', function(){
             expect(sc.stringAdd("//;\n1;2")).to.equal(3)
         })
         it('check negatives throwing msg', function(){
-            expect(() => sc.stringAdd("1,-1,2,-3")).to.throw("Negatives not allowed: -1,-3,")
+            expect(() => sc.stringAdd("1,-1,2,-3")).to.throw("Negatives not allowed: -1,-3")
         })
         it('check add number bigger than 1000', function(){
             expect(sc.stringAdd("2,1002,3,2000")).to.equal(5)
@@ -33,6 +33,9 @@ describe ('StringCalculator', function(){
         })
         it('check add with multiple delimiters', function(){
             expect(sc.stringAdd("//[,][%]\n2,2%2")).to.equal(6)
+        })
+        it('check add multiple delimiters any length', function(){
+            expect(sc.stringAdd("//[;;][|||][:]\n2|||2;;2:2")).to.equal(8)
         })
     })
 })
--- stringCalculator.ts
+++ stringCalculator.ts
@@ -3,14 +3,15 @@ export class StringCalculator {
     stringAdd(numbers: string): number {
         var sum: number = 0
 
-            if (numbers.search("-") != -1) {
-                var err: string = "Negatives not allowed: "
-                var neg = numbers.split("-")
-                for (var i = 1; i < neg.length; i++)   //erster nach split -> leerstring..
-                    err += "-" + neg[i][0] + ",";
-                throw err;
+        if (numbers.search("-") != -1) {
+            var err: string = "Negatives not allowed: "
+            var neg = numbers.split("-")
+            for (var i = 1; i < neg.length; i++)   //erster nach split -> leerstring..
+                err += "-" + neg[i][0] + ",";
+            err = err.substring(0, err.length - 1)
+            throw err;
 
-            }
+        }
 
         if (numbers.length == 1 && Number(numbers) <= 1000)
             sum += Number(numbers)
@@ -21,53 +22,47 @@ export class StringCalculator {
         else if (numbers.charAt(0) == "/" && numbers.charAt(1) == "/") {
             var newString: string
             var delimiter: string
-            var delLen:number=0
+            var delLen: number = 0
             newString = numbers.substr(numbers.lastIndexOf("\n") + 1)
 
             if (numbers.lastIndexOf("\n") == 3)
                 delimiter = numbers.charAt(2)
-            else if(numbers.indexOf("[")==numbers.lastIndexOf("["))
-                delimiter = numbers.substring(numbers.indexOf("[") + 1, numbers.indexOf("]"))
-            else{
-                var delTmp:string=""
-                for(var i=0; i<numbers.length;i++){
-                    if(numbers.charAt(i)=="["){
-                        i++
-                        for(var k=i;numbers.charAt(k)!="]";k++)
-                            delTmp+=numbers.charAt(i)
-                    }
+            else {
+                var delTmp: string = ""
+                var reg = /(?<=\[)(.*?)(?=\])/g
+                var dels = numbers.match(reg)
+                if (dels != null) {
+                    for (var f in dels)
+                        if (dels[f].includes("|"))
+                            delTmp += "\\" + dels[f] + "|"
+                        else
+                            delTmp += dels[f] + "|"
                 }
-                delimiter="["+delTmp+"]"
-                delLen=delimiter.length
+
+                delimiter = "[" + delTmp + "]"
+                delLen = delimiter.length
             }
-            if(delimiter.length==delLen)
+            if (delimiter.length == delLen)
                 var strs = newString.split(new RegExp(delimiter))
             else
                 var strs = newString.split(delimiter)
+
             for (var j in strs) {
                 if (Number(strs[j]) <= 1000)
                     sum += Number(strs[j])
             }
         }
+
         else {
-            var strs = numbers.split(/[\n,]/)
+            var strs = numbers.split(/[\n|,]/)
             for (var j in strs) {
                 if (Number(strs[j]) <= 1000)
                     sum += Number(strs[j])
             }
         }
-
-
         return sum
     }
-
 }
 
 var obj = new StringCalculator()
-console.log(obj.stringAdd("//[,][;]\n2;2,1"))
-//var str4:string="[;]asd[||]asd[:]"
-/*if(str4.indexOf("[")==str4.lastIndexOf("["))
-    console.log("nur eine klammer")
-else
-    console.log("mehrere klammern")
- */
+console.log(obj.stringAdd("//[;]\n1;2;1"))
